0f7e91536439e90e95f4d3158788808173af9944
Swanand-Wagh <swanandwagh7@gmail.com>
2021-04-12 02:35:27 +0530

    Basic Structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import LeftContainer from "./Pages/LeftContainer";
+import MainContainer from "./Pages/MainContainer";
 
 function App() {
   return (
     <>
-      <div className="App"></div>
+      <div className="App">
+        <LeftContainer />
+        <MainContainer />
+      </div>
     </>
   );
 }
