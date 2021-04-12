efb25905aff98f9b06d01ad7e1233e0b3eb0fe6e
Swanand-Wagh <swanandwagh7@gmail.com>
2021-04-13 00:29:15 +0530

    added font-awesome icons & re-structured Profile

--- src/Components/Left Container/Profile.js
+++ src/Components/Left Container/Profile.js
@@ -6,17 +6,19 @@ function Profile() {
   return (
     <>
       <div className="profile">
-        <Image
-          className="dp"
-          src={process.env.PUBLIC_URL + "/assets/dp.png"}
-          roundedCircle
-        />
+        <div className="img-border">
+          <Image
+            className="dp"
+            src={process.env.PUBLIC_URL + "/assets/dp.png"}
+            roundedCircle
+          />
+        </div>
         <div>
           <p
             style={{
               textAlign: "center",
               fontSize: "36px",
-              letterSpacing: "1.25px",
+              letterSpacing: "1.15px",
               fontWeight: "700",
               marginBottom: "0",
             }}
@@ -29,24 +31,15 @@ function Profile() {
           >
             Web Designer
           </p>
-          <div style={{ margin: "15px 0px" }} className="text-center ml-4">
+          <div style={{ margin: "18.5px 0px" }} className="text-center ml-4">
             <a href="https://www.linkedin.com/in/swanandwagh1208/">
-              <Image
-                className="profile-links"
-                src={process.env.PUBLIC_URL + "/assets/linkedin.svg"}
-              />
+              <i class="fab fa-linkedin-in profile-links"></i>
             </a>
             <a href="https://github.com/Swanand-Wagh">
-              <Image
-                className="profile-links"
-                src={process.env.PUBLIC_URL + "/assets/github.svg"}
-              />
+              <i class="fab fa-github profile-links"></i>
             </a>
             <a href="https://www.instagram.com/swanandaaaa/">
-              <Image
-                className="profile-links"
-                src={process.env.PUBLIC_URL + "/assets/instagram.svg"}
-              />
+              <i class="fab fa-instagram profile-links"></i>
             </a>
           </div>
         </div>
@@ -55,7 +48,8 @@ function Profile() {
         </Button>
         <p className="made-by text-center">
           Designed by Alias
-          <br />© 2021 All rights reserved
+          <br />
+          <i class="far fa-copyright"></i> 2021 All rights reserved
         </p>
       </div>
     </>
