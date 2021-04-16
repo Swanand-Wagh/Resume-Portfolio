366a02fbca8313c187f10e42839822427a62497b
Swanand-Wagh <swanandwagh7@gmail.com>
2021-04-17 01:15:53 +0530

    created FunFacts component

--- src/Components/Main Container/about/FunFacts.js
+++ src/Components/Main Container/about/FunFacts.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 import "./about.css";
 
 function FunFacts() {
   return (
     <>
-      <div className="fun-facts"></div>
+      <div className="fun-facts">
+        <p className="pt-4 pb-2 what-i-do">
+          Fun <span>FACTS</span>
+        </p>
+        <Row>
+          <Col>
+            <div className="fact text-center">
+              <i class="fas fa-paper-plane abt-icons"></i>
+              <p className="what-i-do-title">Origami Artist</p>
+              <span></span>
+            </div>
+          </Col>
+          <Col>
+            <div className="fact text-center">
+              <i class="fas fa-pizza-slice abt-icons"></i>
+              <p className="what-i-do-title">Foodie</p>
+              <span></span>
+            </div>
+          </Col>
+          <Col>
+            <div className="fact text-center">
+              <i class="fas fa-skating abt-icons"></i>
+              <p className="what-i-do-title">Sports Enthusiast</p>
+              <span></span>
+            </div>
+          </Col>
+        </Row>
+      </div>
     </>
   );
 }
