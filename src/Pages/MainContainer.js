b321142b642276ac0a0c5a4fa63b8b34b8cca289
Swanand-Wagh <swanandwagh7@gmail.com>
2021-04-19 09:44:22 +0530

    added orange circles to vertical divider in resume

--- src/Pages/MainContainer.js
+++ src/Pages/MainContainer.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Home from "../Components/Main Container/Home";
-import "../App.css";
 import About from "../Components/Main Container/about/About";
+import Resume from "../Components/Main Container/resume/Resume";
+import "../App.css";
 
 function MainContainer() {
   return (
     <>
       <div className="main-container pl-2">
-        <Home />
-        <About />
+        {/* <Home />
+        <About /> */}
+        <Resume />
       </div>
     </>
   );
