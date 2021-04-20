dde3d5a6ea760dbcbe5d9f199f1d79b51cfaffbe
Swanand-Wagh <swanandwagh7@gmail.com>
2021-04-21 02:37:01 +0530

    Others component ready

---
+++ src/Components/Main Container/resume/Others.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./resume.css";
+
+function Others() {
+  return (
+    <>
+      <div className="pl-5">
+        <p className="pt-4 pb-2 what-i-do">Others</p>
+        <div className="others">
+          <div className="others-card">Bootstrap</div>
+          <div className="others-card">Objective C</div>
+          <div className="others-card">Python</div>
+          <div className="others-card">Java</div>
+          <div className="others-card">Git</div>
+          <div className="others-card">Github</div>
+          <div className="others-card">Hosting</div>
+          <div className="others-card">Data Structures</div>
+          <div className="others-card">Problem Solving</div>
+          <div className="others-card">Time Management</div>
+          <div className="others-card">Teaching</div>
+          <div className="others-card">Entrepreneurship</div>
+          <div className="others-card">Team Work</div>
+          <div className="others-card">Communication</div>
+          <div className="others-card">Flexibility</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Others;
